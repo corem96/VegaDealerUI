174f1542841ff6d5fc8344674ada59926f673d0a
Jorge Escamilla <corem96@gmail.com>
2019-05-17 05:08:56 -0600

    Initial Commit including  new component and route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
+import { AppRoutingModule } from './components/app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehicleFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/app-routing/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
+import { VehicleFormComponent } from "../vehicle-form/vehicle-form.component";
+
+const routes: Routes = [
+  { path: 'vehicles/new', component: VehicleFormComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
